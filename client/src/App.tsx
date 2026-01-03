f2e922cd3805341d90a19eb9fc94693d69bfe9bf
speakaiagency-hue <speakai.agency@gmail.com>
2026-01-03 10:32:44 -0300

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,30 +3,15 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import Layout from "./components/Layout";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Prompt from "./pages/Prompt";
-import ImagePage from "./pages/Image";
-import VideoPage from "./pages/Video";
-import Profile from "./pages/Profile";
-import NotFound from "./pages/not-found";
-import { ProtectedRoute } from "./components/ProtectedRoute";
+import NotFound from "@/pages/not-found";
 
-function Router() {
+function AppRouter() {
   return (
     <Switch>
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/" component={() => <Layout><Home /></Layout>} />
-      <Route path="/chat" component={() => <ProtectedRoute><Layout><Chat /></Layout></ProtectedRoute>} />
-      <Route path="/prompt" component={() => <ProtectedRoute><Layout><Prompt /></Layout></ProtectedRoute>} />
-      <Route path="/image" component={() => <ProtectedRoute><Layout><ImagePage /></Layout></ProtectedRoute>} />
-      <Route path="/video" component={() => <ProtectedRoute><Layout><VideoPage /></Layout></ProtectedRoute>} />
-      <Route path="/profile" component={() => <ProtectedRoute><Layout><Profile /></Layout></ProtectedRoute>} />
-      <Route component={() => <Layout><NotFound /></Layout>} />
+      {/* Add pages below */}
+      {/* <Route path="/" component={Home}/> */}
+      {/* Fallback to 404 */}
+      <Route component={NotFound} />
     </Switch>
   );
 }
@@ -36,7 +21,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router />
+        <AppRouter />
       </TooltipProvider>
     </QueryClientProvider>
   );
